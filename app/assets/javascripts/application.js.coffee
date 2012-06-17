f3c9c136105e1a48fc2d1d7180297e9deb5337cb
Chris Nelson <superchrisnelson@gmail.com>
2012-06-17 17:37:24 -0400

    initial checkin, backtastic gem added and backbone:install ran.  Jasminerice is up and running

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,9 @@
+#
+#= require jquery
+#= require jquery_ujs
+#= require underscore
+#= require backbone
+#= require backbone_rails_sync
+#= require backbone_datalink
+#= require backbone/cookbook
+#= require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = 'dd297ddd883064cd821cfa8a42e1bfe239a049bbb8aa0dc8f0218f3c229fe119f3ef9b05fa4cfd35319644d55ec2477efde5c27e9c7f5a125bf0573ad6735a5d'
